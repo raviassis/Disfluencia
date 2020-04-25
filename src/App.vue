7ba496d7f7e4c8f55f0a7e984e42d9d3296fcc50
raviassis <ravi.g.assis@gmail.com>
2020-04-25 09:39:24 -0300

    config vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <SessaoDisfuncao />
+  </div>
+</template>
+
+<script>
+import SessaoDisfuncao from './components/SessaoDisfuncao.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SessaoDisfuncao,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AmostraFala.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="amostraFala">
+        <div class="form-group">
+            <textarea 
+                v-bind:value="value"
+                v-on:input="$emit('input', $event.target.value)"
+                class="form-control textArea"
+                placeholder="Registre aqui a amostra da fala" >
+            </textarea>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AmostraFala',
+  props: {
+    value: String,
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.amostraFala, .amostraFala > div{
+    height: 100%;
+}
+
+.textArea{
+    width: 100%;
+    height: 100%;
+}
+</style>
