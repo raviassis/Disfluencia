7fa2be46257b061e5e89c1b619e95507baff58b3
Dell <Dell@DESKTOP-H7B1SI8>
2020-06-03 18:43:26 -0300

    RF-08

--- src/views/newSession/NewSession.vue
+++ src/views/newSession/NewSession.vue
@@ -1,53 +1,72 @@
 <template>
-    <Session v-model="session" v-on:save="save"/>
+  <Session v-model="session" v-on:save="save" v-on:apagaSessao="apagaSessao" />
 </template>
 <script>
 import Session from '../../components/session/Session';
 import Transcriptor from '../../services/transcriptorService';
 export default {
-    name: 'NewSession',
-    components: {
-        Session
+  name: 'NewSession',
+  components: {
+    Session,
+  },
+  data: function() {
+    return {
+      session: {
+        name: '',
+        speechSample: '',
+        annotation: '',
+      },
+    };
+  },
+  methods: {
+    save() {
+      const session = this.createRequestBodyNewSession();
+      this.$http
+        .post('/sessions', session)
+        .then(() => {
+          alert('Sessão salva com sucesso!');
+          this.$router.push('/');
+        })
+        .catch((err) => {
+          if (err.response.status == 422) {
+            this.tratarErroValidacao(err.response.data);
+          } else {
+            alert('Ocorreu um erro inesperado ao salvar a sessão!');
+          }
+        });
     },
-    data: function() {
-        return {
-            session: {
-                name: '',
-                speechSample: '',
-                annotation: ''
-            },
-        };
+    apagaSessao() {
+      const session = this.createRequestBodyDeleteSession();
+      this.$http
+        .delete('/sessions', session)
+        .then(() => {
+          alert('Sessão excluída com sucesso!');
+        })
+        .catch((err) => {
+          if (err.response.status == 422) {
+            this.tratarErroValidacao(err.response.data);
+          } else {
+            alert('Ocorreu um erro inesperado ao excluir a sessão!');
+          }
+        });
     },
-    methods: {
-        save() {
-            const session = this.createRequestBodyNewSession();
-            this.$http.post('/sessions', session)
-                    .then(() => {
-                        alert('Sessão salva com sucesso!');
-                        this.$router.push('/');
-                    })
-                    .catch((err) => {
-                        if(err.response.status == 422) {
-                            this.tratarErroValidacao(err.response.data);
-                        }
-                        else {                            
-                            alert('Ocorreu um erro inesperado ao salvar a sessão!');
-                        }
-                    });
-        },
-        createRequestBodyNewSession(){
-            return {
-                name: this.session.name,
-                speechSample: this.session.speechSample,
-                annotation: this.session.annotation,
-                transcription: Transcriptor.transcript(this.session.speechSample)
-            };
-        },
-        tratarErroValidacao(err){
-            const msg = err.errors?.map(e => e.message).reduce((s1, s2) => s1 + '\n' + s2, '');
-            alert(msg);
-        },
-
+    createRequestBodyNewSession() {
+      return {
+        name: this.session.name,
+        speechSample: this.session.speechSample,
+        annotation: this.session.annotation,
+        transcription: Transcriptor.transcript(this.session.speechSample),
+      };
     },
-}
+    createRequestBodyDeleteSession() {
+      return {
+        _id: this.session._id,
+      };
+    },
+    tratarErroValidacao(err) {
+      const msg = err.errors?.map((e) => e.message).reduce((s1, s2) => s1 + '\n' + s2, '');
+      alert(msg);
+    },
+  },
+};
 </script>
